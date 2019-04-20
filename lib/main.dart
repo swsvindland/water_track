503d440e621250ee9f3bf512d3960a148215d4ce
swsvindland <swsvindland@gmail.com>
2019-04-20 15:08:56 -0700

    change file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,14 @@
 import 'package:flutter/material.dart';
 import "package:flutter_auth_buttons/flutter_auth_buttons.dart";
 import 'auth.dart';
+import 'login.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MainApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutterbase'),
-          backgroundColor: Colors.amber,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GoogleSignInButton(
-                onPressed: () => authService.googleSignIn()
-              ),
-              MaterialButton(
-                onPressed: () => authService.signOut(),
-                color: Colors.red,
-                textColor: Colors.black,
-                child: Text('Logout'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-class UserProfile extends StatefulWidget {
-  @override
-  UserProfileState createState() => UserProfileState();
-}
-
-class UserProfileState extends State<UserProfile> {
-  Map<String, dynamic> _profile;
-  bool _loading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    authService.profile
-      .listen((state) => setState(() => _profile = state));
-
-    authService.loading
-      .listen((state) => setState(() => _loading = state));
-  }
+class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(20.0),
-          child: Text(_profile.toString()),
-        ),
-        Text(_loading.toString())
-      ],
-    );
+    return new Container();
   }
 }
