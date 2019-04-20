307c39c2a80c6906caca76740badda198a0ec4ed
swsvindland <swsvindland@gmail.com>
2019-04-20 15:17:38 -0700

    finalized home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,9 +7,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(),
-      body: new Container(),
-      floatingActionButton: new FloatingActionButton(onPressed: null),
+      appBar: new AppBar(
+        title: new Text(this.title),
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('You have pushed the button this many times: ',),
+              new Text('0'),
+            ],
+          )
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () => print('PRESSED!'),
+        child: new Icon(Icons.add),
+      ),
     );
   }
 }
