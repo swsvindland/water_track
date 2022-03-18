1954257d3e202720afd60ca9eaaad569fb8f57e5
Sam Svindland <samsvindland@Sams-MacBook-Pro.local>
2022-03-18 12:29:06 -0400

    update version

--- lib/layouts/about.dart
+++ lib/layouts/about.dart
@@ -40,7 +40,7 @@ class AboutPage extends StatelessWidget {
                   style: TextStyle(color: Colors.white, fontSize: 16),
                 ),
                 Text(
-                  '1.6.0',
+                  '2.0.0',
                   style: TextStyle(color: Colors.white, fontSize: 12),
                 ),
               ],
