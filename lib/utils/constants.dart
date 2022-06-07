6072181b9def17e9f627acf471552e460bb4b02d
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-06-07 16:44:22 -0400

    update colors a bit

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -7,3 +7,17 @@ const int sm = 600;
 const int md = 900;
 const int lg = 1200;
 const int xl = 1536;
+
+const background = Color(0xff053F5C);
+const primaryVeryLight = Color(0xffEBFCFF);
+const primaryLight = Color(0xff9FE7F5);
+const primary = Color(0xff4198B5);
+const primaryDark = Color(0xff155E83);
+
+const secondaryLight = Color(0xffF7AD19);
+const secondary = Color(0xffF27F0C);
+const secondaryDark = Color(0xffBD5F00);
+
+const ternaryLight = Color(0xffFFF9EE);
+const ternary = Color(0xffFFEECD);
+const ternaryDark = Color(0xffFCD78F);
